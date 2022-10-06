97fc930aa0ad4a630d8010c8a0fd51cd27e72099
elissabenedict <68104833+elissabenedict@users.noreply.github.com>
2022-10-06 13:05:02 +0800

    RegisterAsVolunter

---
+++ RegisterAsVolunteer.js
@@ -0,0 +1,72 @@
+// Sign Up button events
+var loginBtn = document.getElementById("signUpBtn");
+loginBtn.addEventListener("click", function (event) {
+  
+  var statusArray = [];
+  statusArray.push(checkUserName());
+  statusArray.push(checkPassword());
+  statusArray.push(checkFullname());
+  statusArray.push(checkEmail());
+  statusArray.push(checkPhone());
+  statusArray.push(checkOccupation());
+  statusArray.push(checkDateOfBirth());
+ 
+  if (statusArray.includes(false)) {
+    event.preventDefault();
+    event.stopPropagation();
+  }
+}, false);
+
+// Check the Username 
+function checkUserName() {
+    var username = document.getElementById("username");
+    var usernameValue = username.value.trim();
+  
+    if(usernameValue != ''){
+      addIsValid(username);
+      return true;
+    }else{
+      addIsInvalid(username);
+      return false;
+    }
+  }
+  
+  // Check the Password format is valid
+  function checkPassword() {
+    var password = document.getElementById("password");
+    var passwordValue = password.value.trim();
+    var regex =  /^[A-Za-z]\w{7,14}$/;
+  
+  
+    if(regex.test(passwordValue)){
+      addIsValid(password);
+      return true;
+    }else{
+      addIsInvalid(password);
+      return false;
+    }
+  }
+
+
+  // Add valid class & removes any invalid class
+function addIsValid(element){
+    if(!element.classList.contains("is-valid")){
+      element.classList.add("is-valid");
+    }
+  
+    if(element.classList.contains("is-invalid")){
+      element.classList.remove("is-invalid");
+    }
+  }
+  
+  
+  // Add invalid class & removes any valid class
+  function addIsInvalid(element){
+    if(!element.classList.contains("is-invalid")){
+      element.classList.add("is-invalid");
+    }
+  
+    if(element.classList.contains("is-valid")){
+      element.classList.remove("is-valid");
+    }
+  }
